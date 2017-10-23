3a68ea8dfd8757c89bbd8fcad82030e66f9a99bf
Aslan <aslan.oskouei@ixup.com>
2017-10-23 20:45:51 +1100

    Add Counter widget

---
+++ client/counter/src/app/counter-widget/counter-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fohm-counter-widget',
+  templateUrl: './counter-widget.component.html',
+  styleUrls: ['./counter-widget.component.css']
+})
+export class CounterWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
