4d8506cc2568e892c78ad2e1254e136ef782bcbd
Aslan <aslan.oskouei@ixup.com>
2017-10-23 21:51:51 +1100

    Add Server

---
+++ server/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        int value = 0;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(value);
+        }
+
+        // GET api/values
+        [HttpPut]
+        public IActionResult Minus()
+        {
+            value--;
+            return Ok(value);
+        }
+
+        // GET api/values
+        [HttpPut]
+        public IActionResult Add()
+        {
+            value++;
+            return Ok(value);
+        }
+
+    }
+}
