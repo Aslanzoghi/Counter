aaeaa6334c2068115b5a8d89fd281eed25f3bb67
Aslan <aslan.oskouei@ixup.com>
2017-10-23 19:23:10 +1100

    Add counter-display component

--- client/counter/src/app/app.module.ts
+++ client/counter/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CounterDisplayComponent } from './counter-display/counter-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterDisplayComponent
   ],
   imports: [
     BrowserModule
